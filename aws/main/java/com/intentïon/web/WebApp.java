d415e05073f1632886cdafcebf54136e4efea4ea
Antony at Polycode <antony@polycode.co.uk>
2025-05-12 20:28:09 +0200

    "Rename project and resources from 'xn--intenton-z2a-com-web' to 'intention-com-web'"

--- aws/main/java/com/intentïon/web/WebApp.java
+++ aws/main/java/com/intentïon/web/WebApp.java
@@ -18,34 +18,6 @@ public class WebApp {
                 .s3UseExistingBucket(Boolean.parseBoolean(System.getenv("USE_EXISTING_BUCKET")))
                 .s3RetainBucket(Boolean.parseBoolean(System.getenv("RETAIN_BUCKET"))) // TODO: Switch to removal policy
                 // TODO: S3 bucket object lifecycle policy (delete after 1 month)
-                // TODO: DLQ postfix
-                // TODO: SQS Queue and DLQ retention period
-                .sqsSourceQueueName(System.getenv("SQS_SOURCE_QUEUE_NAME"))
-                .sqsReplayQueueName(System.getenv("SQS_REPLAY_QUEUE_NAME"))
-                .sqsDigestQueueName(System.getenv("SQS_DIGEST_QUEUE_NAME"))
-                .sqsDigestQueueArn(System.getenv("SQS_DIGEST_QUEUE_ARN"))
-                .sqsUseExistingDigestQueue(Boolean.parseBoolean(System.getenv("USE_EXISTING_DIGEST_QUEUE")))
-                .sqsRetainDigestQueue(Boolean.parseBoolean(System.getenv("RETAIN_DIGEST_QUEUE"))) // TODO: Switch to removal policy
-                .offsetsTableName(System.getenv("OFFSETS_TABLE_NAME"))
-                // TODO: Offsets table partition key
-                // TODO: Offsets table stack removal policy
-                // TODO: Offsets table TTL (1 month)
-                .projectionsTableName(System.getenv("PROJECTIONS_TABLE_NAME"))
-                // TODO: Projections table partition key
-                // TODO: Projections table stack removal policy
-                // TODO: Projections table TTL (1 month)
-                .lambdaEntry(System.getenv("LAMBDA_ENTRY"))
-                // TODO: Lambda LogGroup prefix
-                .replayBatchLambdaFunctionName(System.getenv("REPLAY_BATCH_LAMBDA_FUNCTION_NAME"))
-                .replayBatchLambdaHandlerFunctionName(System.getenv("REPLAY_BATCH_LAMBDA_HANDLER_FUNCTION_NAME"))
-                // TODO: Lambda timeout
-                .sourceLambdaFunctionName(System.getenv("SOURCE_LAMBDA_FUNCTION_NAME"))
-                .sourceLambdaHandlerFunctionName(System.getenv("SOURCE_LAMBDA_HANDLER_FUNCTION_NAME"))
-                // TODO: Lambda timeout
-                .replayLambdaFunctionName(System.getenv("REPLAY_LAMBDA_FUNCTION_NAME"))
-                .replayLambdaHandlerFunctionName(System.getenv("REPLAY_LAMBDA_HANDLER_FUNCTION_NAME"))
-                // TODO: Lambda timeout
-                // TODO: As properties not variables: Enable/disable versioning (also allowing unlimited concurrency because we always read the latest state)
                 .build();
 
         CfnOutput.Builder.create(stack, "EventsBucketArn")
@@ -54,46 +26,6 @@ public class WebApp {
 
         CfnOutput.Builder.create(stack, "EventsS3AccessRoleArn")
                 .value(stack.s3AccessRole.getRoleArn())
-                .build();
-
-        CfnOutput.Builder.create(stack, "SourceQueueUrl")
-                .value(stack.sourceQueue.getQueueUrl())
-                .build();
-
-        CfnOutput.Builder.create(stack, "ReplayQueueUrl")
-                .value(stack.replayQueue.getQueueUrl())
-                .build();
-
-        CfnOutput.Builder.create(stack, "DigestQueueUrl")
-                .value(stack.digestQueue.getQueueUrl())
-                .build();
-
-        CfnOutput.Builder.create(stack, "OffsetsTableArn")
-                .value(stack.offsetsTable.getTableArn())
-                .build();
-
-        CfnOutput.Builder.create(stack, "ProjectionsTableArn")
-                .value(stack.projectionsTable.getTableArn())
-                .build();
-
-        CfnOutput.Builder.create(stack, "ReplayBatchLambdaArn")
-                .value(stack.replayBatchLambda.getFunctionArn())
-                .build();
-
-        CfnOutput.Builder.create(stack, "ReplayBatchLambdaLogGroupArn")
-                .value(stack.replayBatchLambdaLogGroup.getLogGroupArn())
-                .build();
-
-        CfnOutput.Builder.create(stack, "ReplayBatchOneOffJobResourceRef")
-                .value(stack.replayBatchOneOffJobResource.getRef())
-                .build();
-
-        CfnOutput.Builder.create(stack, "SourceLambdaArn")
-                .value(stack.sourceLambda.getFunctionArn())
-                .build();
-
-        CfnOutput.Builder.create(stack, "ReplayLambdaArn")
-                .value(stack.replayLambda.getFunctionArn())
                 .build();
 
         app.synth();
--- aws/test/java/com/intentïon/web/WebStackTest.java
+++ aws/test/java/com/intentïon/web/WebStackTest.java
@@ -17,26 +17,9 @@ public class WebStackTest {
                 .s3ObjectPrefix("test/")
                 .s3UseExistingBucket(false)
                 .s3RetainBucket(false)
-                .sqsSourceQueueName("intention-com-web-source-queue-test")
-                .sqsReplayQueueName("intention-com-web-replay-queue-test")
-                .sqsDigestQueueName("intention-com-web-digest-queue-test")
-                // TODO: The digest queue ARN should be optional and omitted in this test.
-                .sqsDigestQueueArn("arn:aws:sqs:eu-west-2:123456789012:intention-com-web-digest-queue-test")
-                .sqsUseExistingDigestQueue(false)
-                .sqsRetainDigestQueue(false)
-                .offsetsTableName("intention-com-web-offsets-table-test")
-                .projectionsTableName("intention-com-web-projections-table-test")
-                .lambdaEntry("src/lib/main.")
-                .replayBatchLambdaFunctionName("intention-com-web-replay-batch-function")
-                .replayBatchLambdaHandlerFunctionName("replayBatchLambdaHandler")
-                .sourceLambdaFunctionName("intention-com-web-source-function")
-                .sourceLambdaHandlerFunctionName("sourceLambdaHandler")
-                .replayLambdaFunctionName("intention-com-web-replay-function")
-                .replayLambdaHandlerFunctionName("replayLambdaHandler")
                 .build();
 
         Template template = Template.fromStack(stack);
-        template.resourceCountIs("AWS::SQS::Queue", 6);
-        template.resourceCountIs("AWS::Lambda::Function", 6);
+        template.resourceCountIs("AWS::S3::Bucket", 2);
     }
 }
